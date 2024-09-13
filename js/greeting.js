75c1e25a15200c2b9df7fb860765f80b481664e4
Leonid Popkov <lpopkov.popkov@yandex.ru>
2024-09-14 01:51:53 +0300

    попытка исправить проблему с неработающим скриптом

--- js/greeting.js
+++ js/greeting.js
@@ -1,19 +1,3 @@
-let tg = window.Telegram.WebApp;
-// Получаем элемент заголовка
-let userNameGreeting = document.getElementById("greeting");
-// Проверяем наличие данных о пользователе
-if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
-    let user = tg.initDataUnsafe.user;
-    let userName = `${user.first_name} ${user.last_name}`;
-    // Устанавливаем имя пользователя в заголовок
-    userNameGreeting.textContent = `Здравствуйте, ${userName}!`;
-} else {
-    // Если данные о пользователе недоступны, выводим сообщение в консоль
-    console.log("Данные о пользователе недоступны.");
-    userNameGreeting.textContent = "Здравствуйте, гость!";
-}
-
-
 let enteringZone = document.getElementById("enter");
 document.getElementById("button-login").addEventListener('click', () => {
     enteringZone.remove();
